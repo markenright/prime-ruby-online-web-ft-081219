3d71c34c81089fd06b7109e8323f89552fa04682
Mark Enright <enright.mark@gmail.com>
2019-08-15 19:32:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(x)
+  
+  if x < 2
+    return false
+   
+  else
+    new_range = (2..(x/2)).to_a
+    new_range.each do |n|
+      if x % n == 0
+        return false
+      end
+    end
+  end
+  
+  return true
+end
+
+
+puts prime?(11)
